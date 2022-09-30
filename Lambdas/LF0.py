266a129678b39eae74a66362edaf84c40b3772ea
binvant <binvantbajwa@gmail.com>
2022-09-29 22:35:35 -0400

    Added LF0

---
+++ Lambdas/LF0.py
@@ -0,0 +1,14 @@
+import json
+
+def lambda_handler(event, context):
+    return{
+        'statusCode' : 200,
+        
+        'headers':{
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            
+        },
+        'body':{'messages':[{'unstructured':{'text':'Application under development. Search functionality will be implemented in Assignment 2.'}, 'type':'unstructured'}]}
+    }
